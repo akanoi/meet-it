8a49e99bbda2cdc9aabb6785afa0459d785d242a
akanoi <aleh.i.kanoika@gmail.com>
2019-03-30 15:24:46 +0300

    Add devise fields for Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ require_relative '../../config/constants'
 
 class User < ApplicationRecord
   include Constants
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true, length: { maximum: NAX_EMAIL_LENGTH },

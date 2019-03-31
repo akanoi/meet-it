b18b613a0b74b9939e295277f6f3ee449bff6bbd
mtuleyko <tuleika.m@profitero.com>
2019-03-31 10:59:46 +0300

    populate users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,13 +65,14 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:date, :datetime, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.require(:event).permit(:date, :datetime, :description)
+  end
 end
